0780f95c8449f25288fbbcabda38d452f785fa3e
nnt0 <20939869+nnt0@users.noreply.github.com>
2020-10-03 00:31:56 +0200

    improve error handling in move_files

--- fily_lib/src/operations/move_files/mod.rs
+++ fily_lib/src/operations/move_files/mod.rs
@@ -1,24 +1,43 @@
-use std::{fs::{canonicalize, rename, create_dir_all}, error::Error, path::Path};
+use std::{fs::{canonicalize, rename, create_dir_all}, error::Error, path::Path, io, fmt};
 use dialoguer::Confirm;
 #[allow(unused_imports)]
 use log::{trace, debug, info, warn, error};
 
+#[derive(Debug)]
+pub enum MoveFilesError {
+    CouldNotCreateMoveToPath(io::Error),
+    MoveToPointsToAFile,
+    IOError(io::Error),
+}
+
+impl Error for MoveFilesError {}
+
+impl fmt::Display for MoveFilesError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 /// Moves all files from one place to another
-pub fn move_files<T, U>(move_to: T, files_to_move: &[U], you_sure_prompt: bool) -> Result<(), Box<dyn Error>> where
+pub fn move_files<T, U>(move_to: T, files_to_move: &[U], you_sure_prompt: bool) -> Result<(), MoveFilesError> where
     T: AsRef<Path>,
     U: AsRef<Path> {
-    if !move_to.as_ref().exists() {
-        create_dir_all(&move_to)?;
+    let move_to = move_to.as_ref();
+    if !move_to.exists() {
+        if let Err(e) = create_dir_all(&move_to) {
+            info!("Failed to create move_to path: {:?} {}", move_to.display(), e);
+            return Err(MoveFilesError::CouldNotCreateMoveToPath(e));
+        }
     }
 
     let move_to = match canonicalize(&move_to) {
         Ok(path) => path,
         Err(e) => {
-            let err_msg = format!("Error accessing {:?} {}", move_to.as_ref().display(), e);
-            info!("{}", err_msg);
-            return Err(Box::from(err_msg));
+            info!("Error accessing {:?} {}", move_to.display(), e);
+            return Err(MoveFilesError::IOError(e));
         }
     };
+
     let files_to_move = {
         let mut files_to_move_canonicalized = Vec::with_capacity(files_to_move.len());
 
@@ -39,16 +58,16 @@ pub fn move_files<T, U>(move_to: T, files_to_move: &[U], you_sure_prompt: bool) 
 
     match move_to.metadata() {
         Ok(metadata) => if metadata.file_type().is_file() {
-            return Err(Box::from("move_to points to a file"));
+            info!("move_to points to a file {:?}", move_to.display());
+            return Err(MoveFilesError::MoveToPointsToAFile);
         }
         Err(e) => {
-            let err_msg = format!("Error accessing {:?} {}", move_to.display(), e);
-            info!("{}", err_msg);
-            return Err(Box::from(err_msg));
+            info!("Error accessing {:?} {}", move_to.display(), e);
+            return Err(MoveFilesError::IOError(e));
         }
     };
 
-    // For some reason we get errors if this is used in a pipe. Not sure why. I'll just uncomment is for now
+    // For some reason we get errors if this is used in a pipe. Not sure why. I'll just uncomment it for now
     // Not sure if this is even useful
     // if you_sure_prompt && !Confirm::new().with_prompt(format!("Moving {} files to {} . Are you sure?", files_to_move.len(), move_to.display())).default(false).interact()? {
     //     debug!("Aborting because of negative prompt response");
@@ -65,7 +84,7 @@ pub fn move_files<T, U>(move_to: T, files_to_move: &[U], you_sure_prompt: bool) 
             }
         });
         match rename(&old_path, &new_path) {
-            Ok(_) => info!("Moved {:?} to {:?}", old_path.display(), new_path.display()),
+            Ok(()) => info!("Moved {:?} to {:?}", old_path.display(), new_path.display()),
             Err(e) => info!("Error moving {:?} {}", old_path.display(), e),
         };
     }
